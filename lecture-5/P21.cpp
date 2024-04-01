af1558aa46575a4ffd20440397cc047aff0cdfbf
ishubarman7 <ishu.barman9067900@gmail.com>
2024-04-02 03:55:50 +0530

    Update P21.cpp

--- lecture-5/P21.cpp
+++ lecture-5/P21.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
                       //if we note give the input parameters and initialize the variables under the curly brases og funtion
                      // then we dont have to intialise the variable at the time of using this funcion again and agin as we see in p20.cpp
-                     
+
 int power()         
 {   
     int x,y;
@@ -28,21 +28,21 @@ int main()
     
 
     int answer = power();
-    cout<< answer << endl;
+    cout<<"Answer is: "<< answer << endl;
     cout<<endl;
    
 
     int answer1 = power();
-    cout<< answer1 << endl;
+    cout<<"Answer is: "<< answer1 << endl;
     cout<<endl;
     
 
     int answer2 = power();
-    cout<< answer2 << endl;
+    cout<<"Answer is: "<< answer2 << endl;
     cout<<endl;
    
 
     int answer3 = power();
-    cout<< answer3 << endl;
+    cout<<"Answer is: "<< answer3 << endl;
     cout<<endl;
 }
