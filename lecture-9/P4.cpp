8e051fc88c786eed8b6528ad03435c347accd759
ishubarman7 <ishu.barman9067900@gmail.com>
2024-04-06 12:44:00 +0530

    Update P4.cpp

--- lecture-9/P4.cpp
+++ lecture-9/P4.cpp
@@ -3,19 +3,26 @@ using namespace std;
 
 int main()
 {
-    int arr[100] = {0};
 
-    for(int i = 0; i < 100; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout<<endl<<endl;
-
-
+            //    only the first element of the array become 1 and rest of the element save the value as 0(zero).
     int arr_1[100] = {1};
 
     for(int i = 0; i < 100; i++)
     {
         cout << arr_1[i] << " ";
     }
+     cout<<endl<<endl;
+
+     
+
+
+                // all the element in this situation become zero but there is a twist in this case.
+                // (first element become 0 because i declare it to be 0 but the rest of element become 0 because there is no declaration for the rest element after declaring first element ).
+    int arr[100] = {0};
+
+    for(int i = 0; i < 100; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    
 }
