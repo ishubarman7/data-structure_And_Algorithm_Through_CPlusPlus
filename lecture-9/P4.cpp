1e858484a526a42a900b9280e4bbf02ca819b8a2
ishubarman7 <ishu.barman9067900@gmail.com>
2024-04-06 12:42:47 +0530

    Update P4.cpp

---
+++ lecture-9/P4.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[100] = {0};
+
+    for(int i = 0; i < 100; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout<<endl<<endl;
+
+
+    int arr_1[100] = {1};
+
+    for(int i = 0; i < 100; i++)
+    {
+        cout << arr_1[i] << " ";
+    }
+}
