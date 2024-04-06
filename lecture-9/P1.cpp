593550cded85a387509d9f117a1f85381438fd1d
ishubarman7 <ishu.barman9067900@gmail.com>
2024-04-06 12:19:20 +0530

    .

---
+++ lecture-9/P1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+
+    for(int i = 0; i < 10; i++)
+    {
+        cout<<arr[i]<<" "<<endl;
+    }
+}
---
+++ lecture-9/P2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[10];
+    for(int i = 0; i < 10; i++)
+    {
+        cout <<"enter the value of "<<i<<"th number of elements"<<endl;
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i<10; i++)
+    {
+        cout << arr[i] << endl;
+    }
+}
