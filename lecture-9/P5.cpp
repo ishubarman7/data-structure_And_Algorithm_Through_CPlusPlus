d334046fa82479a84fc37f078fb06c02283fd1dc
ishubarman7 <ishu.barman9067900@gmail.com>
2024-04-07 09:46:02 +0530

    Create P5.cpp

---
+++ lecture-9/P5.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char arr[5];
+
+    for(int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+         cout<<"printing the array"<<endl;
+    for(int i = 0;i < 5;i++)
+    {
+        cout << arr[i] << endl;
+    }
+}
