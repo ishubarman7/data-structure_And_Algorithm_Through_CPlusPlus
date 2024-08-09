efa419bd3c9a563f2a1e5d6af88d69fe0c3be8d0
ishubarman7 <ishu.barman9067900@gmail.com>
2024-08-09 08:56:45 +0000

    ghhhhhh

---
+++ lecture-9/test123.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main()
+{
+    char arr[5];
+
+    for(int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+         cout<<"Printing the array "<<endl;
+    for(int i = 0;i < 5;i++)
+    {
+        cout << arr[i] << endl;
+    }
+}
